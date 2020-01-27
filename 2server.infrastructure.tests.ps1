a3110d78aeab0641fde1a9f8a518a5f0fb73d513
James O'Neill <P10506111@capita.co.uk>
2020-01-27 09:41:31 +0000

    Move deploy into a template

--- 2server.infrastructure.tests.ps1
+++ 2server.infrastructure.tests.ps1
@@ -1,24 +1,56 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$RGName , 
+    [ValidateNotNullOrEmpty()]
+    [string]$Subscription = $env:SUBSCRIPTION_ID
+  
+)
+if (-not (Get-Module -ListAvailable Az.Network ))  {Install-Module -Name Az.Network   -Force -SkipPublisherCheck}
+if (-not (Get-Module -ListAvailable Az.Resources)) {Install-Module -Name Az.Resources -Force -SkipPublisherCheck}
+if (-not (Get-Module -ListAvailable Az.Security )) {Install-Module -Name Az.Security  -Force -SkipPublisherCheck}
+if (-not (Get-Module -ListAvailable Az.Compute  )) {Install-Module -Name Az.Compute   -Force -SkipPublisherCheck}
 
-#Az.Resources    Az.Compute   Az.Network
-Get-AzResourceGroup -Name 'ServerProvisionTesting-221'
 
-$vm = Get-AzVM -ResourceGroupName  'ServerProvisionTesting-221' -Name TESTVMDEPLOY1 
-$vm.name
-$vm.location
-$vm.HardwareProfile.vmsize
-$vm.OSProfile.ComputerName
-$vm.OSProfile.AdminUsername
-$vm.StorageProfile.ImageReference.sku
-
-(Get-AzPublicIpAddress -ResourceGroupName "ServerProvisionTesting-221").name
-
-$azvn = Get-AzVirtualNetwork -ResourceGroupName  'ServerProvisionTesting-221'
-$azvn.AddressSpace.AddressPrefixes
-$azvn.Subnets | Select-Object name,addressprefix
-$azvn.name
-
-$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName 'ServerProvisionTesting-221'
-$nsg.name
-$nsg.SecurityRules[0].name
-$nsg.SecurityRules[0].DestinationPortRange[0]
-
+describe "Configuration" {
+    $rg     =  Get-AzResourceGroup -Name $RGName
+    $params = (Get-Content .\server.parameters.json | ConvertFrom-Json)
+    $vmname = $params.parameters.vm1Name.value
+    it 'created the resource group.            ' {
+        $rg.ProvisioningState                           | Should      -Be "Succeeded"
+        $rg.ResourceId                                  | Should      -BeLike ("*$subscription*")
+    }
+    it 'created the VM.                        ' {
+        $vmname                                         | Should -not -BeNullOrEmpty  
+        $vm = Get-AzVM -ResourceGroupName  $RGName -Name $vmname
+        $vm.name                                        | Should      -Be $vmname
+        $vm.location                                    | Should      -Be $rg.Location
+        $vm.OSProfile.ComputerName                      | Should      -Be $vmname  
+        $vm.OSProfile.AdminUsername                     | Should      -Be $params.parameters.adminUsername.value
+        $vm.NetworkProfile.NetworkInterfaces.id         | Should -not -BeNullOrEmpty  
+        $nic = (Get-AzNetworkInterface -ResourceId  $vm.NetworkProfile.NetworkInterfaces.id)
+        $nic.IpConfigurations[0].PublicIpAddress.Id     | Should -not -BeNullOrEmpty  
+        #$vm.HardwareProfile.vmsize
+        #$vm.StorageProfile.ImageReference.sku
+    }
+    it 'created the virtual network.           ' {
+        $azvn = Get-AzVirtualNetwork -ResourceGroupName $RGName
+        $azvn.count                                     | Should       -Be 1
+        $azvn.AddressSpace.AddressPrefixes.Count        | Should       -Be 1
+        $azvn.AddressSpace.AddressPrefixes[0]           | Should       -Be      '10.0.0.0/16'
+        $azvn.Subnets.AddressPrefix                     | Should       -Contain '10.0.0.0/24'
+        $azvn.Subnets.AddressPrefix                     | Should       -Contain '10.0.1.0/24'
+    }
+    it 'created the Network Security Group.    ' {
+        $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName  $RGName
+        $nsg.count                                      | Should       -Be 1
+        $nsg.SecurityRules[0].name                      | Should       -Be 'default-allow-3389'
+        $nsg.SecurityRules[0].DestinationPortRange[0]   | Should       -Be '3389' 
+    }
+    it 'enabled RDP Access                     ' {
+        $ArmDeploymentOutput = $env:ARM_OUTPUT | convertfrom-json
+        $ArmDeploymentOutput                            | Should -not -BeNullOrEmpty  
+        Test-Connection -TCPPort 3389 -TargetName $ArmDeploymentOutput.hostname.value -Quiet |
+                                                          Should-     -Be $true
+    }
+}
--- config-vm.ps1
+++ config-vm.ps1
@@ -6,10 +6,13 @@ if (-not (Get-Module -ListAvailable Az.Resources)) {Install-Module -Name Az.Reso
 if (-not (Get-Module -ListAvailable Az.Security )) {Install-Module -Name Az.Security  -Force -SkipPublisherCheck}
 if (-not (Get-Module -ListAvailable Az.Compute  )) {Install-Module -Name Az.Compute   -Force -SkipPublisherCheck}
 
-$rg                   = Get-AzResourceGroup -name $rgname
-$rg | Out-String
+$rg                   =  Get-AzResourceGroup -name $rgname
 $vmname               = (Get-Content .\server.parameters.json | ConvertFrom-Json ).parameters.vm1name.value
 $vm                   =  Get-AzVM -ResourceGroupName $rgname -Name $vmname
+$rg | Out-String
+$vm | Out-String
+Get-ChildItem Env:\ | Format-Table -AutoSize
+
 <#
 $locname              = $rg.Location
 $durationString       = "PT3H" # ISO 8601 spec for duration https://en.wikipedia.org/wiki/ISO_8601#Durations   [System.Xml.XmlConvert]::ToString($TimeSpan)
@@ -35,7 +38,7 @@ foreach ($p in $ports) {
 $JitPolicyVMList      += $NewVMPolicy
 $jitPolicy            = Set-AzJitNetworkAccessPolicy -Kind "Basic" -Location $locname -Name $JITPolName -ResourceGroupName $rgname -VirtualMachine $JitPolicyVMList
 #>
-$remoteCommand = @'
+Set-Content   -Path .\remote.ps1 -Value @'
 tzutil.exe /s            "GMT Standard Time"
 tzutil.exe /g
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" /v ShutDownReasonOn /t REG_DWORD /d 0 /f
@@ -47,8 +50,7 @@ Import-Module           -Name NetSecurity
 Get-NetFirewallRule     -Name winrm*public  | Set-NetFirewallRule -RemoteAddress any
 Get-NetFirewallRule     -Name winrm*public  | Out-File c:\users\public\fwrule.txt
 
-Get-Service -Name winrm
-Get-PSSessionConfiguration
+
 Get-NetFirewallRule -Name 'WINRM-HTTP-In-*'
 
 Import-Module           -Name PackageManagement,PowerShellGet
@@ -62,6 +64,10 @@ Install-Module          -Name xActiveDirectory
 Install-Module          -Name xpendingreboot
 Install-Module          -Name xsmbshare
 Install-Module          -Name xwindowsupdate
+
+Get-Service -Name winrm
+
+Get-PSSessionConfiguration
 
 Import-Module           -Name pki
 $newCert = New-SelfSignedCertificate -Type Custom -TextExtension @("2.5.29.37={text}1.3.6.1.4.1.311.80.1" ) -KeyUsage DataEncipherment,KeyEncipherment -Subject "cn=$env:COMPUTERNAME" -CertStoreLocation "cert:\LocalMachine\My" -FriendlyName "Desired State Configuration"
@@ -82,14 +88,26 @@ Configuration Baseline {
  }
 $null = Baseline             # if it doesn't exist, this will create the folder for the DSC Cert
 Set-DscLocalConfigurationManager -Path "C:\Program Files\WindowsPowerShell\Baseline" -ComputerName LocalHost
-Get-Get-DscLocalConfigurationManager
+Get-DscLocalConfigurationManager
 Remove-Item  -Force              -Path "C:\Program Files\WindowsPowerShell\Baseline\*.cer" -ErrorAction SilentlyContinue | Out-Null
 $null = Export-Certificate   -FilePath "C:\Program Files\WindowsPowerShell\Baseline\$env:COMPUTERNAME.cer" -Cert $newCert
 Pop-Location
 '@
-$vm | Out-String
+$remoteResult = Invoke-AzVMRunCommand -ResourceGroupName $rgname -VMName $vmname -ScriptPath .\remote.ps1 -CommandId "RunPowerShellScript"
+Remove-Item   -Path  .\remote.ps1
 
-Set-Content -Path .\remote.ps1 -Value $remoteCommand
-Invoke-AzVMRunCommand -ResourceGroupName $rgname -VMName $vmname -ScriptPath .\remote.ps1 -CommandId "RunPowerShellScript"
-Remove-Item .\remote.ps1
-Stop-azvm -ResourceGroupName $rgname -Name $vmname -Force -NoWait
+$resultDir    = Join-Path      -Path $env:BUILD_ARTIFACTSTAGINGDIRECTORY -ChildPath "results"
+if (-not       (Test-Path      -Path $resultDir -PathType Container )) {
+                 New-Item      -Path $resultDir -ItemType Directory 
+}
+$XmlPath      = Join-Path      -Path $resultDir -ChildPath 'remoteResult.xml'
+Export-Clixml -Depth 5         -Path $XmlPath   -InputObject $remoteResult
+
+$textPath     = Join-Path      -Path $resultDir  -ChildPath 'vm_and_rg.txt'
+$rg             | Out-file -FilePath $textPath 
+$vm             | Out-file -FilePath $textPath   -Append
+$env:ARM_OUTPUT | Out-file -FilePath $textPath   -Append
+$XmlPath     = Join-Path       -Path $resultDir  -ChildPath 'vm.xml'
+Export-Clixml -Depth 5         -Path $XmlPath    -InputObject $vm
+
+Stop-AzVm -ResourceGroupName $rgname -Name $vmname -Force -NoWait
