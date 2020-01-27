88793eeede03f617f65cd6da6037ae3967273ec6
jhoneill <jhoneill@waitrose.com>
2020-01-27 18:35:20 +0000

    Fix test at end of pipeline

--- 2server.infrastructure.tests.ps1
+++ 2server.infrastructure.tests.ps1
@@ -1,10 +1,10 @@
 param (
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [string]$RGName , 
+    [string]$RGName ,
     [ValidateNotNullOrEmpty()]
     [string]$Subscription = $env:SUBSCRIPTION_ID
-  
+
 )
 if (-not (Get-Module -ListAvailable Az.Network ))  {Install-Module -Name Az.Network   -Force -SkipPublisherCheck}
 if (-not (Get-Module -ListAvailable Az.Resources)) {Install-Module -Name Az.Resources -Force -SkipPublisherCheck}
@@ -21,15 +21,15 @@ describe "Configuration" {
         $rg.ResourceId                                  | Should      -BeLike ("*$subscription*")
     }
     it 'created the VM.                        ' {
-        $vmname                                         | Should -not -BeNullOrEmpty  
+        $vmname                                         | Should -not -BeNullOrEmpty
         $vm = Get-AzVM -ResourceGroupName  $RGName -Name $vmname
         $vm.name                                        | Should      -Be $vmname
         $vm.location                                    | Should      -Be $rg.Location
-        $vm.OSProfile.ComputerName                      | Should      -Be $vmname  
+        $vm.OSProfile.ComputerName                      | Should      -Be $vmname
         $vm.OSProfile.AdminUsername                     | Should      -Be $params.parameters.adminUsername.value
-        $vm.NetworkProfile.NetworkInterfaces.id         | Should -not -BeNullOrEmpty  
+        $vm.NetworkProfile.NetworkInterfaces.id         | Should -not -BeNullOrEmpty
         $nic = (Get-AzNetworkInterface -ResourceId  $vm.NetworkProfile.NetworkInterfaces.id)
-        $nic.IpConfigurations[0].PublicIpAddress.Id     | Should -not -BeNullOrEmpty  
+        $nic.IpConfigurations[0].PublicIpAddress.Id     | Should -not -BeNullOrEmpty
         #$vm.HardwareProfile.vmsize
         #$vm.StorageProfile.ImageReference.sku
     }
@@ -45,12 +45,12 @@ describe "Configuration" {
         $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName  $RGName
         $nsg.count                                      | Should       -Be 1
         $nsg.SecurityRules[0].name                      | Should       -Be 'default-allow-3389'
-        $nsg.SecurityRules[0].DestinationPortRange[0]   | Should       -Be '3389' 
+        $nsg.SecurityRules[0].DestinationPortRange[0]   | Should       -Be '3389'
     }
     it 'enabled RDP Access                     ' {
         $ArmDeploymentOutput = $env:ARM_OUTPUT | convertfrom-json
-        $ArmDeploymentOutput                            | Should -not -BeNullOrEmpty  
+        $ArmDeploymentOutput                            | Should -not -BeNullOrEmpty
         Test-Connection -TCPPort 3389 -TargetName $ArmDeploymentOutput.hostname.value -Quiet |
-                                                          Should-     -Be $true
+                                                          Should      -Be $true
     }
 }
